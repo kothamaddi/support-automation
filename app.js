afca03ed601606329e180566298d17aa0da75480
kothamaddi <siva.kothamaddi@gmail.com>
2023-04-07 14:39:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,73 +1,52 @@
-// AWS SDK dependencies
 const AWS = require('aws-sdk');
+const S3 = new AWS.S3({apiVersion: '2006-03-01'});
+const lambda = new AWS.Lambda({apiVersion: '2015-03-31'});
+const BUCKET_NAME = 'your-bucket-name';
 
-// Configuration
-const bucketName = 'your-bucket-name';
-const region = 'your-region';
-const lambdaFunctionName = 'your-lambda-function-name';
-
-// Get the S3 service object
-const s3 = new AWS.S3({ region });
-
-// Get the Lambda service object
-const lambda = new AWS.Lambda({ region });
-
-// Update the UI with the progress message
-function showProgressMessage() {
-  const progressMessage = document.getElementById('progress-message');
-  progressMessage.innerText = 'Uploading file and waiting for analysis...';
-
-  const progressIcon = document.getElementById('progress-icon');
-  progressIcon.style.display = 'inline-block';
-}
-
-// Update the UI with the completion message
-function showCompletionMessage() {
-  const progressMessage = document.getElementById('progress-message');
-  progressMessage.innerText = 'File has been uploaded and analyzed.';
-
-  const progressIcon = document.getElementById('progress-icon');
-  progressIcon.style.display = 'none';
-}
-
-// Upload a file to S3
 function uploadFile() {
-  // Get the file from the file input element
-  const fileInput = document.getElementById('file-input');
-  const file = fileInput.files[0];
+  const file = document.getElementById('file-input').files[0];
+  const fileName = file.name;
 
-  // Get a unique key for the object we're uploading
-  const key = `${Date.now()}-${file.name}`;
+  if (!fileName) {
+    alert('Please select a file to upload.');
+    return;
+  }
 
-  // Create the S3 object params
-  const s3Params = {
-    Bucket: bucketName,
-    Key: key,
+  // Show loading spinner
+  document.getElementById('loading-spinner').style.display = 'block';
+
+  // Upload file to S3 bucket
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: fileName,
     Body: file
   };
 
-  // Update the UI with the progress message
-  showProgressMessage();
-
-  // Upload the file to S3
-  s3.upload(s3Params, function(err, data) {
+  S3.upload(params, function(err, data) {
     if (err) {
-      console.log('Error uploading file:', err);
+      alert('Error uploading file: ' + err);
       return;
     }
 
-    console.log('File uploaded to S3:', data);
-
-    // Call the Lambda function to process the file
-    const lambdaParams = {
-      FunctionName: lambdaFunctionName,
-      Payload: JSON.stringify({ bucket: bucketName, key })
+    const payload = {
+      fileName: fileName
     };
 
-    lambda.invoke(lambdaParams, function(err, data) {
+    // Invoke Lambda function to process file
+    lambda.invoke({
+      FunctionName: 'your-lambda-function-name',
+      Payload: JSON.stringify(payload)
+    }, function(err, data) {
       if (err) {
-        console.log('Error invoking Lambda function:', err);
+        alert('Error invoking Lambda function: ' + err);
         return;
       }
 
-      console.log('Lambda function invoked:', data
+      // Show success message and hide loading spinner
+      document.getElementById('success-message').style.display = 'block';
+      document.getElementById('loading-spinner').style.display = 'none';
+    });
+  });
+}
+
+document.getElementById('upload-button').addEventListener('click', uploadFile);
